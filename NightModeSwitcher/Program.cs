060aaa22068da11e402d978817c058dae7d3975a
Stef B <regendo@users.noreply.github.com>
2019-07-26 14:25:54 +0200

    Toggle Light/Dark mode from C#

--- NightModeSwitcher/Program.cs
+++ NightModeSwitcher/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 
 namespace NightModeSwitcher
 {
@@ -10,6 +6,11 @@ namespace NightModeSwitcher
     {
         static void Main(string[] args)
         {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", true);
+            bool wasLightTheme = key.GetValue("AppsUseLightTheme").ToString() == "1";
+            int newValue = wasLightTheme ? 0 : 1;
+            key.SetValue("AppsUseLightTheme", newValue);
+            key.SetValue("SystemUsesLightTheme", newValue);
         }
     }
 }
