08df5ba7199e03fa7d1376f9c5e54eb6454ff8ef
Stef B <regendo@users.noreply.github.com>
2019-07-26 14:52:37 +0200

    Command line arguments to turn nightmode on/off

--- NightModeSwitcher/Program.cs
+++ NightModeSwitcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using System;
 
 namespace NightModeSwitcher
 {
@@ -6,9 +7,34 @@ namespace NightModeSwitcher
     {
         static void Main(string[] args)
         {
+            const string usageMsg = "Usage: NightModeSwitcher.exe [on|off]";
             RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", true);
-            bool wasLightTheme = key.GetValue("AppsUseLightTheme").ToString() == "1";
-            int newValue = wasLightTheme ? 0 : 1;
+            int newValue;
+
+            switch (args.Length)
+            {
+                case 0:
+                    bool wasLightTheme = key.GetValue("AppsUseLightTheme").ToString() == "1";
+                    newValue = wasLightTheme ? 0 : 1;
+                    break;
+                case 1:
+                    switch (args[0])
+                    {
+                        case "on":
+                            newValue = 1;
+                            break;
+                        case "off":
+                            newValue = 0;
+                            break;
+                        default:
+                            Console.Error.WriteLine(usageMsg);
+                            return;
+                    }
+                    break;
+                default:
+                    Console.Error.WriteLine(usageMsg);
+                    return;
+            }
             key.SetValue("AppsUseLightTheme", newValue);
             key.SetValue("SystemUsesLightTheme", newValue);
         }
